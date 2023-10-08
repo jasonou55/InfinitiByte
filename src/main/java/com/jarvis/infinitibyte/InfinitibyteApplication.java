0009f0b4122651fbe7fb9a08f6429fedf67f766d
Jason.Ou <jason.ou55@gmail.com>
2023-10-08 16:12:08 -0700

    Initial

---
+++ src/main/java/com/jarvis/infinitibyte/InfinitibyteApplication.java
@@ -0,0 +1,13 @@
+package com.jarvis.infinitibyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinitibyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinitibyteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jarvis/infinitibyte/InfinitibyteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvis.infinitibyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinitibyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
